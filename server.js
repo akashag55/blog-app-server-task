2b876034fc459ccb1aa4d3d87396ceb901f50c9a
Akash Agarwal <51333155+akashag55@users.noreply.github.com>
2022-12-16 20:11:41 +0530

    vercel update

--- server.js
+++ server.js
@@ -1,10 +1,29 @@
-const jsonServer = require('json-server');
-const server = jsonServer.create();
-const router = jsonServer.router('db.json'); // <== Will be created later
-const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 8000; // <== You can change the port
+// const jsonServer = require('json-server');
+// const server = jsonServer.create();
+// const router = jsonServer.router('db.json'); // <== Will be created later
+// const middlewares = jsonServer.defaults();
+// const port = process.env.PORT || 8000; // <== You can change the port
 
-server.use(middlewares);
-server.use(router);
+// server.use(middlewares);
+// server.use(router);
 
-server.listen(port);
+// server.listen(port);
+// See https://github.com/typicode/json-server#module
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const router = jsonServer.router('db.json')
+const middlewares = jsonServer.defaults()
+
+server.use(middlewares)
+// Add this before server.use(router)
+// server.use(jsonServer.rewriter({
+//     '/api/*': '/$1',
+//     '/blog/:resource/:id/show': '/:resource/:id'
+// }))
+server.use(router)
+server.listen(8000, () => {
+    console.log('JSON Server is running')
+})
+
+// Export the Server API
+module.exports = server
