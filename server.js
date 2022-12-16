46052c68da21b65683e0d425a73f2b338dc04edb
Akash Agarwal <51333155+akashag55@users.noreply.github.com>
2022-12-16 20:31:39 +0530

    changes in server file

--- server.js
+++ server.js
@@ -1,29 +1,13 @@
-// const jsonServer = require('json-server');
-// const server = jsonServer.create();
-// const router = jsonServer.router('db.json'); // <== Will be created later
-// const middlewares = jsonServer.defaults();
-// const port = process.env.PORT || 8000; // <== You can change the port
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
 
-// server.use(middlewares);
-// server.use(router);
-
-// server.listen(port);
-// See https://github.com/typicode/json-server#module
-const jsonServer = require('json-server')
-const server = jsonServer.create()
-const router = jsonServer.router('db.json')
-const middlewares = jsonServer.defaults()
-
-server.use(middlewares)
-// Add this before server.use(router)
-// server.use(jsonServer.rewriter({
-//     '/api/*': '/$1',
-//     '/blog/:resource/:id/show': '/:resource/:id'
-// }))
-server.use(router)
+server.use(middlewares);
+server.use(router);
 server.listen(8000, () => {
-    console.log('JSON Server is running')
-})
+  console.log("JSON Server is running");
+});
 
 // Export the Server API
-module.exports = server
+module.exports = server;
